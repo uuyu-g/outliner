88950f87b850bc733047a5e8bf1e39b9af20ce7b
uuyu-g <36399275+uuyu-g@users.noreply.github.com>
2024-06-02 16:28:13 +0900

    hotkeyのサンプル追加

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,13 @@
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import { useHotkeys } from 'react-hotkeys-hook';
+
+const ExampleComponent = () => {
+  const [count, setCount] = useState(0);
+  useHotkeys('mod+k', () => setCount(count + 1), [count]);
+
+  return <p>Pressed {count} times.</p>;
+};
 
 function App() {
   const [text, setText] = useState('');
@@ -15,6 +23,7 @@ function App() {
       <h1>Hello from React!</h1>
       <button onClick={handleClick}>Open Dialog</button>
       <p>{text}</p>
+      <ExampleComponent />
     </main>
   );
 }
